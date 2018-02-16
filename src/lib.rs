dd6a1dd160a4766ffe2bbf6a45a14fac293cb012
David Roundy <roundyd@physics.oregonstate.edu>
2018-02-16 05:50:24 -0800

    relicense as MIT/APACHE

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2018 David Roundy
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 //! A very easy to use library for
 //! [interning](https://en.wikipedia.org/wiki/String_interning)
 //! strings or other data in rust.  Interned data is very efficient to
