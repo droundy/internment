35966593c5708bdef989902a68eab09ebba2e698
David Roundy <roundyd@physics.oregonstate.edu>
2020-06-21 17:14:39 -0700

    bump version number

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 David Roundy
+// Copyright 2018,2020 David Roundy
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
