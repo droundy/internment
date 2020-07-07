0f73696cd05d3cb9655e4d916e5e44c1f8af067a
David Roundy <roundyd@physics.oregonstate.edu>
2020-07-07 15:19:36 -0700

    tweak arc-sanitize to make it maybe more likely to trigger on travis-ci

--- examples/arc-sanitize.rs
+++ examples/arc-sanitize.rs
@@ -1,14 +1,24 @@
 use internment::{ArcIntern, Intern, LocalIntern};
 
 fn main() {
+    println!("^^^^^^^^^^^^^^^^^^^^^^");
     Intern::new(0i64);
     LocalIntern::new(0i64);
     ArcIntern::new(0i64);
     arc_interner::ArcIntern::new(0i64);
+    let mut threads = Vec::new();
     for _ in 1..10000 {
-        std::thread::spawn(|| {
-            ArcIntern::new(1);
-        });
+        threads.push(std::thread::spawn(|| {
+            ArcIntern::new(1i64);
+        }));
+        threads.push(std::thread::spawn(|| {
+            let _x = ArcIntern::new(1i64);
+            std::thread::sleep(std::time::Duration::from_nanos(1));
+        }));
     }
-    println!("all good");
+    for x in threads.into_iter() {
+        x.join().unwrap();
+    }
+    println!("vvvvvvvvvvvvvvvvvvvvv");
+    println!("all good\n\n\n\n");
 }
