6770f4d3066fc831b027f33086e07b78bd7589da
David Roundy <roundyd@physics.oregonstate.edu>
2022-03-10 15:01:41 -0800

    start moving ArenaIntern code

--- src/arena.rs
+++ src/arena.rs
@@ -61,3 +61,77 @@ impl<T: Eq + std::hash::Hash> Arena<T> {
         }
     }
 }
+
+
+impl<T> Default for Arena<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+
+impl<'a, T> AsRef<T> for ArenaIntern<'a, T> {
+    fn as_ref(&self) -> &T {
+        self.pointer
+    }
+}
+
+impl<'a, T> ArenaIntern<'a, T> {
+    fn get_pointer(&self) -> *const T {
+        self.pointer as *const T
+    }
+}
+
+// #[cfg(feature = "arena")]
+// create_impls_no_new!(ArenaIntern, arenaintern_impl_tests, ['a], [Eq, Hash], [Eq, Hash]);
+
+// #[cfg(feature = "arena")]
+// impl<'a, T: Debug> Debug for ArenaIntern<'a, T> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+//         std::fmt::Debug::fmt(&self.get_pointer(), f)?;
+//         f.write_str(" : ")?;
+//         self.as_ref().fmt(f)
+//     }
+// }
+
+// #[cfg(feature = "arena")]
+// mod test_arena {
+//     use super::*;
+//     #[test]
+//     fn eq_string() {
+//         let arena = Arena::<&'static str>::new();
+//         assert_eq!(arena.intern("hello"), arena.intern("hello"));
+//         assert_ne!(arena.intern("goodbye"), arena.intern("farewell"));
+//     }
+//     #[test]
+//     fn display() {
+//         let arena = Arena::<&'static str>::new();
+//         let world = arena.intern("world");
+//         println!("Hello {}", world);
+//     }
+//     #[test]
+//     fn debug() {
+//         let arena = Arena::<&'static str>::new();
+//         let world = arena.intern("world");
+//         println!("Hello {:?}", world);
+//     }
+//     #[test]
+//     fn can_clone() {
+//         let arena = Arena::<&'static str>::new();
+//         assert_eq!(arena.intern("hello").clone(), arena.intern("hello"));
+//     }
+//     #[test]
+//     fn has_borrow() {
+//         let arena = Arena::<Option<String>>::new();
+//         let x = arena.intern(None);
+//         let b: &Option<String> = x.borrow();
+//         assert_eq!(b, arena.intern(None).as_ref());
+//     }
+//     #[test]
+//     fn has_deref() {
+//         let arena = Arena::<Option<String>>::new();
+//         let x = arena.intern(None);
+//         let b: &Option<String> = x.as_ref();
+//         assert_eq!(b, arena.intern(None).deref());
+//     }
+// }
